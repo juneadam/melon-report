96922eb3bb856ba154ff70a75bccba3ac96d16bf
June Balter <junedoeswork@gmail.com>
2022-12-14 20:25:33 -0600

    initial commit

---
+++ produce_summary.py
@@ -0,0 +1,45 @@
+file_list = ["um-deliveries-day-1.txt", "um-deliveries-day-2.txt", "um-deliveries-day-3.txt"]
+for file in file_list:
+    day = file_list.index(file) + 1
+    print(day)
+
+# print("Day 1")
+# the_file = open("um-deliveries-day-1.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
