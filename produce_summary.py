c1b3749db6646b46c25dfb2be7b6cf04e085280c
June Balter <junedoeswork@gmail.com>
2022-12-14 20:33:19 -0600

    refactored file to eliminate repetition

--- produce_summary.py
+++ produce_summary.py
@@ -1,20 +1,20 @@
 file_list = ["um-deliveries-day-1.txt", "um-deliveries-day-2.txt", "um-deliveries-day-3.txt"]
+
 for file in file_list:
     day = file_list.index(file) + 1
-    print(day)
+    print(f"Day {day}")
 
-# print("Day 1")
-# the_file = open("um-deliveries-day-1.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
+    the_file = open(file)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-#     melon = words[0]
-#     count = words[1]
-#     amount = words[2]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-#     print(f"Delivered {count} {melon}s for total of ${amount}")
-# the_file.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
 
 
 # print("Day 2")
@@ -24,8 +24,8 @@ for file in file_list:
 #     words = line.split('|')
 
 #     melon = words[0]
-#     count = words[1]
-#     amount = words[2]
+#     count = words[0]
+#     amount = words[0]
 
 #     print(f"Delivered {count} {melon}s for total of ${amount}")
 # the_file.close()
@@ -38,8 +38,8 @@ for file in file_list:
 #     words = line.split('|')
 
 #     melon = words[0]
-#     count = words[1]
-#     amount = words[2]
+#     count = words[0]
+#     amount = words[0]
 
 #     print(f"Delivered {count} {melon}s for total of ${amount}")
 # the_file.close()
